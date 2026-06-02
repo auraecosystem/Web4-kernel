3dbba6c1908efd1d01812f596b46278e238e8fdb
auraecosystem <sandraisom@icloud.com>
2026-06-02 21:10:08 +0100

    Create bash.pas

---
+++ bash.pas
@@ -0,0 +1,7 @@
+echo "# Web4-kernel" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/auraecosystem/Web4-kernel.git
+git push -u origin main
